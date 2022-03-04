45545452327371e6b3acafb2bf2e3ec170abf295
nezjutsu <seankral9@gmail.com>
2022-03-03 19:38:09 -0500

    update server.js/ add room route/ index.js

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
-const routes = require('./routes');
+//const routes = require('./controllers');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
+const session = require('express-session');
 
 // import sequelize connection
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
@@ -25,9 +29,6 @@ const hbs = exphbs.create({ helpers });
 app.engine ('handlebars', hbs.engine);
 app.set ('view engine', 'handlebars');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -37,5 +38,4 @@ app.use(require('./controllers/'));
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
-}); 
-.
+});
