54a33dd28bf121d4bca9d189908968d93b6a6e7f
Aaron Neslin <aneslin125@gmail.com>
2022-03-04 16:40:18 -0500

    added log in route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -76,3 +76,52 @@ router.put("/:id", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+//delete user
+router.delete("/:id", (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "no user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+//log in route
+router.post("/login", (req, res) => {
+  User.findOne({
+    where: { email: req.body.email },
+  })
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(400).json({ message: "no user with that email address" });
+        return;
+      }
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: " incorrect password" });
+        return;
+      }
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.email = dbUserData.email;
+        req.session.loggedIn = true;
+
+        res.json({ user: dbUserData, message: "you are now logged in" });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
