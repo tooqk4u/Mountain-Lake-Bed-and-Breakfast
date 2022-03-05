13b2d9ec4c96d8b8bed7954282f43474d1eadfe6
Aaron Neslin <aneslin125@gmail.com>
2022-03-05 17:34:40 -0500

    added public room list view route

---
+++ controllers/rooms-routes.js
@@ -0,0 +1,24 @@
+const router = require("express").Router();
+const { Room, Amenities } = require("../models");
+
+router.get("/", (req, res) => {
+  Room.findAll({
+    attributes: [
+      "id",
+      "room_name",
+      "room_price",
+      "room_img",
+      "room_description",
+    ],
+  })
+    .then((dbRoomData) => {
+      const rooms = dbRoomData.map((room) => room.get({ plain: true }));
+      res.render("rooms", { rooms });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
