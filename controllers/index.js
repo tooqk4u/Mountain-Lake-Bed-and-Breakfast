61a9dd4f40f829b50dfb904ae46b8ef3601de7ab
tooqk4u <tooqk4u@protonmail.com>
2022-03-06 21:28:57 -0500

    index.js changes we made on zoom

--- controllers/index.js
+++ controllers/index.js
@@ -1,21 +1,10 @@
-lconst router = require('express').Router();
-
-const apiRoutes = require('./api');
-const homeRoute = require('./home-routes')
-
-router.use('/api', apiRoutes);
-router.use('/', homeRoute)
-const RoomsRoute = require("./rooms-routes.js")
-router.use('/api', apiRoutes);
-router.use("/rooms", RoomsRoute)
-
-const homeRoute = require('./home-routes')
-router.use('/', homeRoute)
-
-const RoomsRoute = require("./rooms-routes.js")
-router.use("/rooms", RoomsRoute)
-
-const RoomDetailsRoute = require("./roomDetail_routes.js")
-router.use("/room-details", RoomDetailsRoute)
-
+const router = require("express").Router();
+const apiRoutes = require("./api");
+router.use("/api", apiRoutes);
+const homeRoute = require("./home-routes");
+router.use("/", homeRoute);
+const RoomsRoute = require("./rooms-routes.js");
+router.use("/rooms", RoomsRoute);
+const RoomDetailsRoute = require("./roomDetail_routes.js");
+router.use("/room-details", RoomDetailsRoute);
 module.exports = router;
