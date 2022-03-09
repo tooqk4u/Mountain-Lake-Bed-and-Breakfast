81b06633d1037e39aefa546e46ca9756f034beee
tooqk4u <tooqk4u@protonmail.com>
2022-03-09 17:00:01 -0500

    Merge branch 'develop' of github.com:tooqk4u/Mountain-Lake-Bed-and-Breakfast into develop

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,32 @@
+const router = require("express").Router();
+const { Room, User, Booking_Dates } = require("../models");
+const { Op } = require("sequelize");
+const dayjs = require("dayjs");
+const withAuth = require('../utils/auth')
+router.get("/", withAuth, (req, res) => {
+  User.findOne({
+    where:{id: req.session.user_id},
+    attributes: { exclude: ["password"] },
+    include: [
+      {
+        model: Booking_Dates,
+        where: {
+          start_date: { [Op.gte]: dayjs() },
+        },
+        include: [{ model: Room, attributes: ["room_name"] }],
+      },
+    ],
+  })
+    .then((dbDashData) => {
+      const dashData = dbDashData.get(({plain: true}))
+      console.log(dashData)
+      res.render("dashboard", { dashData, loggedIn: true });
+    })
+
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
