595929a7033ca31a21c61b9806bb8c0e04f5ec90
Aaron Neslin <aneslin125@gmail.com>
2022-03-09 20:55:43 -0500

    cleaned nav bar repair sign up

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -18,7 +18,7 @@ async function loginFormHandler(event) {
       if (response.ok) {
           document.location.replace('/');
       } else {
-          alert(response.statusText);
+          alert("Incorrect Password");
       }
   }
 }
